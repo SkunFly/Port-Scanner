263a9f550fa21efdd16092839ef358d7b7b14e1d
André Pereira <pereirasmandre@gmail.com>
2017-07-30 03:33:58 +0100

    main

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "network.h"
 
 #define MAX_PORT 4500
 
 
-void portscanner(char* ip){
+void portscanner(char* ip, int init, int end){
   int socket = socket_creation();
   int open_ports[MAX_PORT];
-  int i = 1, index = 0;
+  int i = init, index = 0;
 
   printf("Starting portscanning...\n");
-  while(i <= MAX_PORT){
+  while(i <= end){
     printf("Port %d: ", i);
     if(connect_to(socket, ip, i)){
       printf("Open\n");
@@ -28,7 +29,22 @@ void portscanner(char* ip){
   puts("");
 }
 
-int main(){
-  portscanner("google.com");
+int main(int argc, char** argv){
+  int init, end;
+  switch (argc) {
+    case 2:
+      portscanner(argv[1], 1, MAX_PORT);
+      break;
+    case 4:
+      init = atoi(argv[2]);
+      end = atoi(argv[3]);
+      if(init > 0 && end > 0 && end >= init && end <=  MAX_PORT)
+        portscanner(argv[1], init, end);
+      break;
+    case 1:
+    case 3:
+    default:
+      printf("Usage: portscanner ip [first port] [last port]\n");
+  }
   return 0;
 }
